2337214c9b5c4c39deaba20a95cd573464c755cb
Fedor Chervinskii <fedor.chervinskii@gmail.com>
2014-12-31 00:13:11 +0300

    new algorithm: kNN

---
+++ Scripts/kNN.m
@@ -0,0 +1,46 @@
+k = 5;
+
+dataSetSize = size(datesData);
+
+testDataSetSize = size(testDatesData);
+
+answerCount = zeros(testDataSetSize);
+
+tic
+for i=1:testDataSetSize
+    kMinRadius = ones(k,1).*100;
+    kMinNeighbours = zeros(k,1);
+    
+    for j=1:dataSetSize
+        
+        if testHours(i) ~= hours(j)
+            continue
+        end
+        
+        radius = ((temp(j)-temp1(i))/41)^2+...
+            ((atemp(j)-testatemp(i))/45)^2+...
+            ((windspeed(j)-windspeed1(i))/57)^2+...
+            ((humidity(j)-humidity1(i))/100)^2+...
+            (weather(j)~=weather1(i))+...
+            (workingday(j)~=workingday1(i))+...
+            (holiday(j)~=holiday1(i))+...
+            (season(j)~=season1(i));
+        index = k;
+        while (radius < kMinRadius(index))
+            temprad = kMinRadius(index);
+            tempind = kMinNeighbours(index);
+            kMinRadius(index) = radius;
+            kMinNeighbours(index) = j;
+            if index<k
+                kMinRadius(index+1) = temprad;
+                kMinNeighbours(index+1) = tempind;
+            end
+            index = index-1;
+            if index == 0
+                break
+            end
+        end    
+    end
+    answerCount(i) = floor(sum(count(kMinNeighbours))/k);
+end
+toc
