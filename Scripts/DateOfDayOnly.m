b10932c026c8b516d0f55587ecd8124d096f3cfd
Nikita Pestrov <ipestrov@gmail.com>
2014-12-30 16:03:01 +0300

    Getting Temp and Weather averages

--- Scripts/DateOfDayOnly.m
+++ Scripts/DateOfDayOnly.m
@@ -4,13 +4,37 @@ hoursInDay = 24;
 hourlySum = zeros(hoursInDay,1);
 hourlyEntries = zeros(hoursInDay,1);
 
+weathersSum = zeros(4,1);
+weathersEntries = zeros(4,1);
+
+tempSum = zeros(42,1);
+tempEntries = zeros(42,1);
+
 for dataPoint = 1:dataSetSize
+    
+    %Hours ? Main Trend
     dataPointHour = hourFromDate(datesData{dataPoint});
     hourlySum(dataPointHour) = hourlySum(dataPointHour) + count(dataPoint);
     hourlyEntries(dataPointHour) = hourlyEntries(dataPointHour)+1;
+    
+    %Seasons
+    
+    %Weather
+    dataPointWeather = weather(dataPoint);
+    weathersSum(dataPointWeather) = weathersSum(dataPointWeather) + count(dataPoint);
+    weathersEntries(dataPointWeather) = weathersEntries(dataPointWeather)+1;
+    
+    %Temperature
+    dataPointTemp = floor(temp(dataPoint)) + 1;
+    tempSum(dataPointTemp) = tempSum(dataPointTemp) + count(dataPoint);
+    tempEntries(dataPointTemp) = tempEntries(dataPointTemp)+1;
+    
+    
 end
 
 hourlyMeans = floor(hourlySum./hourlyEntries);
+weatherMeans = floor(weathersSum./weathersEntries)
+tempMeans = floor(tempSum./tempEntries)
 
 testDataSetSize = size(testDatesData);
 
@@ -19,3 +43,4 @@ answerCount = zeros(testDataSetSize);
 for i = 1:testDataSetSize
     answerCount(i) = hourlyMeans(hourFromDate(testDatesData{i}));
 end
+
