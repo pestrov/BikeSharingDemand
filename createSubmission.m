8cd1bf8eebc6c1fd2331a231697ce11959cbaa07
Fedor Chervinskii <fedor.chervinskii@gmail.com>
2014-12-30 14:30:09 +0300

    submission file generation

---
+++ createSubmission.m
@@ -0,0 +1,14 @@
+function createSubmission(filename,datesData,count)
+
+fid = fopen(filename, 'w') ;
+fprintf(fid, '%s,', 'datetime') ;
+fprintf(fid, '%s\n', 'count') ;
+
+N = numel(datesData);
+
+for i=2:N 
+    fprintf(fid, '%s,', datesData{i});
+    fprintf(fid, '%i\n', count(i));
+end
+
+fclose(fid);
