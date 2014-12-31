f226a24865fb39948176d641c8fd7c6d8b5eeddc
Fedor Chervinskii <fedor.chervinskii@gmail.com>
2014-12-31 16:31:59 +0300

    leave one out validation
    different technics of weighting neighbours:
    
    ones
    linear(i)
    exponential(i)

---
+++ Scripts/LOO.m
@@ -0,0 +1,99 @@
+k = 20;
+
+dataSetSize = size(datesData);
+
+hour = zeros(dataSetSize);
+for i=1:dataSetSize
+    hour(i)=hourFromDate(datesData{i});
+end
+
+w = [0,0,0,0,1,1,1,1];
+sumLogarithmicError = 0;
+tic
+for i=1:dataSetSize
+    kMinRadius = ones(k,1).*100;
+    kMinNeighbours = zeros(k,1);
+    
+    for j=1:dataSetSize
+        
+        if (hour(i) ~= hour(j))||(i==j)
+            continue
+        end
+        
+        radius = ...
+            w(1)*(temp(j)-temp(i))^2+...
+            w(2)*(atemp(j)-atemp(i))^2+...
+            w(3)*(windspeed(j)-windspeed(i))^2+...
+            w(4)*(humidity(j)-humidity(i))^2+...
+            w(5)*(weather(j)~=weather(i))+...
+            w(6)*(workingday(j)~=workingday(i))+...
+            w(7)*(holiday(j)~=holiday(i))+...
+            w(8)*(season(j)~=season(i));
+        index = k;
+        while (radius < kMinRadius(index))
+            temprad = kMinRadius(index);
+            tempind = kMinNeighbours(index);
+            kMinRadius(index) = radius;
+            kMinNeighbours(index) = j;
+            if index<k
+                kMinRadius(index+1) = temprad;
+                kMinNeighbours(index+1) = tempind;
+            end
+            index = index-1;
+            if index == 0
+                break
+            end
+        end    
+    end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % neighbour weight is 1
+    
+    %answerCount = floor(sum(count(kMinNeighbours))/k); 
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % neighbour weight is (k+1-i)/k
+%     sum = 0;
+%     sumWeights = 0;
+%     for index=1:k
+%         weight = (k+1-index)/k;
+%         sum = sum + weight*count(kMinNeighbours(index));
+%         sumWeights = sumWeights + weight;
+%     end
+%     answerCount = sum/sumWeights;
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % neighbour weight is q^i
+    q = 0.7;
+    weight = 1;
+    sum = 0;
+    sumWeights = 0;
+    for index=1:k
+        sum = sum + weight*count(kMinNeighbours(index));
+        sumWeights = sumWeights + weight;
+        weight = weight*q;
+    end
+    answerCount = sum/sumWeights;
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % neighbour weight is reverse proportional to the radius
+    
+%     meanRadius = mean(kMinRadius);
+%     if meanRadius == 0
+%         answerCount(i) = floor(sum(count(kMinNeighbours))/k);
+%     else
+%         meanCountSum = 0;
+%         weightsSum = 0;
+%         for neighboor = 1:k
+%             meanCountSum = meanCountSum + count(kMinNeighbours(neighboor)) * (1 - kMinRadius(neighboor)/meanRadius)^2;
+%             weightsSum = weightsSum + (1 - kMinRadius(neighboor)/meanRadius)^2;
+%         end
+%         answerCount(i) = meanCountSum/weightsSum;
+%     end
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    logarithmicError = (log(answerCount+1)-log(count(i)+1))^2;
+    sumLogarithmicError = sumLogarithmicError + logarithmicError;
+
+end
+RMSLE = sqrt(sumLogarithmicError/dataSetSize(1))
+toc
